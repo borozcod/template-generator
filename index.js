21299b9e9886e8029e96cac499cda3e6d2b80af1
Bryan Orozco <bryanorozcoweb@gmail.com>
2019-01-27 09:36:14 -0800

    Template Generator

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const chalk = require('chalk');
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function (input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  }
+];
+
+const CURR_DIR = process.cwd();
+
+inquirer.prompt(QUESTIONS)
+  .then(answers => {
+    const projectChoice = answers['project-choice'];
+    const projectName = answers['project-name'];
+    const templatePath = `${__dirname}/templates/${projectChoice}`;
+    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+    createDirectoryContents(templatePath, projectName);
+    console.log(chalk.bgCyan.black(" All done! :D "));
+});
+
+function createDirectoryContents (templatePath, newProjectPath) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach(file => {
+    const origFilePath = `${templatePath}/${file}`;
+
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, 'utf8');
+
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, 'utf8');
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+    }
+  });
+}
